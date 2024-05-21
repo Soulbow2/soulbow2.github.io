6f015de599652f1c4dc0924d0a849cd945431cd1
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-05-20 20:38:44 -0500

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -188,7 +188,7 @@ element.style.background="red";}
 else if (element.style.background==="red") {
 element.style.background="forestgreen";}}
 
-function notation2() 
+function notation2() {
 if (aone.style.background==="red") {
 aone.style.background="forestgreen";
 element.style.background='red';}
@@ -212,7 +212,7 @@ gseven.style.background="forestgreen";
 element.style.background='red';}
 else if (height.style.background==="red") {
 height.style.background="forestgreen";
-element.style.background='red';
+element.style.background='red';}
 else if (element.style.background==="forestgreen") {
 element.style.background="red";}
 else if (element.style.background==="red") {
