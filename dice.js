e5bc31c66bf0315180ac13a1643d6a542d73f731
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-05-31 22:42:18 -0500

    Create dice.js

---
+++ dice.js
@@ -0,0 +1,20 @@
+function selectTile(element) {
+if 
+(element.style.background==="white") {
+node.innhtml="1";}
+else if { 
+(element.style.background==="silver") {
+node.innhtml="2";}
+else if { 
+(element.style.background==="grey") {
+node.innhtml="3";}
+else if { 
+(element.style.background==="lightgrey") {
+node.innhtml="4";}
+else if { 
+(element.style.background==="lightcyan") {
+node.innhtml="5";}
+else if { 
+(element.style.background==="lightgreen") {
+node.innhtml="6";}
+}
