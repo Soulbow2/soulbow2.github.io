0bceffad5ced5d36e9bfa1d48d8f6caeba53d8f3
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-05-31 23:15:30 -0500

    Update dice.js

--- dice.js
+++ dice.js
@@ -4,19 +4,19 @@ function selectTile(element) {
 if 
 (element.style.background==="white") {
 node.innHTML="1";}
-else if { 
+else if 
 (element.style.background==="silver") {
 node.innHTML="2";}
-else if { 
+else if 
 (element.style.background==="grey") {
 node.innHTML="3";}
-else if { 
+else if 
 (element.style.background==="lightgrey") {
 node.innerHTML="4";}
-else if { 
+else if 
 (element.style.background==="lightcyan") {
 node.innerHTML="5";}
-else if { 
+else if 
 (element.style.background==="lightgreen") {
 node.innerHTML="6";}
 }
