abb0a331146cfa7849e4b20d44c996a2425e1286
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-02 15:46:41 -0500

    Update dice.js

--- dice.js
+++ dice.js
@@ -11,4 +11,39 @@ document.getElementById("node").innHTML="5";}
 function selectTileSix() {
 document.getElementById("node").innHTML="6";}
 
-
+document.getElementById("a1").addEventListener("click", selectTileOne);
+document.getElementById("b1").addEventListener("click", selectTileTwo);
+document.getElementById("c1").addEventListener("click", selectTileThree);
+document.getElementById("d1").addEventListener("click", selectTileFour);
+document.getElementById("e1").addEventListener("click", selectTileFive);
+document.getElementById("f1").addEventListener("click", selectTileSix);
+document.getElementById("a2").addEventListener("click", selectTileOne);
+document.getElementById("b2").addEventListener("click", selectTileTwo);
+document.getElementById("c2").addEventListener("click", selectTileThree);
+document.getElementById("d2").addEventListener("click", selectTileFour);
+document.getElementById("e2").addEventListener("click", selectTileFive);
+document.getElementById("f2").addEventListener("click", selectTileSix);
+document.getElementById("a3").addEventListener("click", selectTileOne);
+document.getElementById("b3").addEventListener("click", selectTileTwo);
+document.getElementById("c3").addEventListener("click", selectTileThree);
+document.getElementById("d3").addEventListener("click", selectTileFour);
+document.getElementById("e3").addEventListener("click", selectTileFive);
+document.getElementById("f3").addEventListener("click", selectTileSix);
+document.getElementById("a4").addEventListener("click", selectTileOne);
+document.getElementById("b4").addEventListener("click", selectTileTwo);
+document.getElementById("c4").addEventListener("click", selectTileThree);
+document.getElementById("d4").addEventListener("click", selectTileFour);
+document.getElementById("e4").addEventListener("click", selectTileFive);
+document.getElementById("f4").addEventListener("click", selectTileSix);
+document.getElementById("a5").addEventListener("click", selectTileOne);
+document.getElementById("b5").addEventListener("click", selectTileTwo);
+document.getElementById("c5").addEventListener("click", selectTileThree);
+document.getElementById("d5").addEventListener("click", selectTileFour);
+document.getElementById("e5").addEventListener("click", selectTileFive);
+document.getElementById("f5").addEventListener("click", selectTileSix);
+document.getElementById("a6").addEventListener("click", selectTileOne);
+document.getElementById("b6").addEventListener("click", selectTileTwo);
+document.getElementById("c6").addEventListener("click", selectTileThree);
+document.getElementById("d6").addEventListener("click", selectTileFour);
+document.getElementById("e6").addEventListener("click", selectTileFive);
+document.getElementById("f6").addEventListener("click", selectTileSix);
