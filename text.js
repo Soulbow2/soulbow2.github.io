a85d472097d4c2f2a38a8cb0a1208ebb47bdca47
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-03 04:26:33 -0500

    Update text.js

--- text.js
+++ text.js
@@ -5,7 +5,7 @@ const element="document.getElementById("element");
 
 app.get(/:userid/html, function (req, res) {
 res.post(req.params.userid) // element
-);}
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
