85c59eaf39640a9e7fcbc5e705d419689f965896
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-03 01:29:55 -0500

    Update text.js

--- text.js
+++ text.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const res = require('express/lib/response');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 127.0. 0.1.;
 
 app.use(express.urlencoded({extended: false}));
 
