2c9d75277056ba48d28b4b27418a7d121be3b58a
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-03 04:05:18 -0500

    Update text.js

--- text.js
+++ text.js
@@ -5,7 +5,7 @@ const port = 8000;
 
 const server = http.createServer(function (req, res) {
 res.writeHead(200, { "Content-Type": "text/plain" });
-res.end("Hello World\n");
+res.send("Hello World");
 });
 
 server.listen(port, hostname, function () {
