d9ac326ba0e9e02bfcb18255d056e713b2344b59
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-03 00:36:24 -0500

    Update chatbox.js

--- chatBox/chatbox.js
+++ chatBox/chatbox.js
@@ -3,3 +3,6 @@
     if(uri === undefined){
       uri = window.location.search;
     }
+ function isObject(input) {
+        return input != null && Object.prototype.toString.call(input) === '[object Object]';
+    }
