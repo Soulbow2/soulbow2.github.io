d5b36f031d16dc1b0c11b4821948bc8c3036b427
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-03 00:37:34 -0500

    Update chatbox.js

--- chatBox/chatbox.js
+++ chatBox/chatbox.js
@@ -6,3 +6,6 @@
  function isObject(input) {
         return input != null && Object.prototype.toString.call(input) === '[object Object]';
     }
+function isNumber(input) {
+        return typeof input === 'number' || Object.prototype.toString.call(input) === '[object Number]';
+    }
