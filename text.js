35173d791c320b222c693627c8631465778d7fda
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-03 02:42:35 -0500

    Update text.js

--- text.js
+++ text.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 function server() {
 app.post(/html.html, function (req, res) {
-res.send("document.getElementById("element").innerHTML= document.getElementById("input").value;");
+res.send("document.getElementById("element").innerHTML= document.getElementById("input").innerHTML;");
 });}
 
 var port = process.env.PORT || 3000;
