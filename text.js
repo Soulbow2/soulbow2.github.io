e6cf2256f41f7c1b9f3e148f077d037aa05ad1cd
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-03 14:08:58 -0500

    Update text.js

--- text.js
+++ text.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get(/html.html:, function (req, res) {
-res.send(document.getElementById("element").background="black");
+res.send(document.getElementById("element").style.background="black");
 })
 
 app.listen(3000)
