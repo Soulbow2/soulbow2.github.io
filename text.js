3a390df84ca25b32df987b949a728f1ac7ede0cc
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-03 05:02:56 -0500

    Update text.js

--- text.js
+++ text.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-const element="document.getElementById("element");
-const input="document.getElementById("input");
-
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
+
+const element="document.getElementById("element");
+const input="document.getElementById("input");
 
 app.get(/:post/html, function (req, res) {
 res.post(req.get.input) // element
