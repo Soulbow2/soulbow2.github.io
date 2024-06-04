31df429b4a23cc10b110c889335fb7826df7469c
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2024-06-04 18:25:25 -0500

    Update text.js

--- text.js
+++ text.js
@@ -1,11 +1,13 @@
 var express = require('express');
+const path = require('path');
 var app = express();
 const PORT = 3000;
 
 app.get('/html.html', (req, res)=>{
-
 res.set('Content-Type', 'text/html');
 res.status(200).send("<h1>This is Live Input</h1>");
+res.sendFile(path.join(__dirname,'image.jpg'));
+app.use('/static', express.static(path.join(__dirname, 'Static Files')))
 });
 
 app.listen(PORT, (error) =>{
