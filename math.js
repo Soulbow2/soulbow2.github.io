36e80dc8d61c869338529d20fc6935272ade50c6
Soulbow2 <138326763+Soulbow2@users.noreply.github.com>
2025-03-05 13:31:53 -0600

    Update math.js

--- math.js
+++ math.js
@@ -3,69 +3,261 @@ const first=document.getElementById("first");
 const second=document.getElementById("second");
 const third=document.getElementById("third");
 const node=document.getElementById("node");
+const sign=document.getElementById("sign");
 
 function blank() {
 btn.innerHTML="1";
 }
 
 function timesTables() {
+const btnCall=document.getElementById("btn");
 const btn = document.getElementById("btn").innerHTML;
 const array=[btn];
-if (node.innerHTML==="0"){
+
+if (node.innerHTML==="0" && sign.innerHTML==="+"){}
+else if (node.innerHTML==="1" && sign.innerHTML==="+") {
+btnCall.innerHTML++;}
+else if (node.innerHTML==="2" && sign.innerHTML==="+") {
+btnCall.innerHTML++;
+btnCall.innerHTML++;}
+else if (node.innerHTML==="3" && sign.innerHTML==="+") {
+btnCall.innerHTML++;
+btnCall.innerHTML++;  
+btnCall.innerHTML++;}
+else if (node.innerHTML==="4" && sign.innerHTML==="+") {
+btnCall.innerHTML++;
+btnCall.innerHTML++;  
+btnCall.innerHTML++;
+btnCall.innerHTML++;  }
+else if (node.innerHTML==="5" && sign.innerHTML==="+") {
+btnCall.innerHTML++;
+btnCall.innerHTML++;  
+btnCall.innerHTML++;
+btnCall.innerHTML++;  
+btnCall.innerHTML++;}
+else if (node.innerHTML==="6" && sign.innerHTML==="+") {
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;}
+else if (node.innerHTML==="7" && sign.innerHTML==="+") {
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;}
+else if (node.innerHTML==="8" && sign.innerHTML==="+") {
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;}
+else if (node.innerHTML==="9" && sign.innerHTML==="+") {
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;}
+else if (node.innerHTML==="10" && sign.innerHTML==="+") {
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;
+btnCall.innerHTML++;}
+
+else if (node.innerHTML==="0" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 0);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="1" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 1);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="2" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 2);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="3" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 3);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="4" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 4);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="5" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 5);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="6" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 6);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="7" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 7);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="8" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 8);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;} 
+else if (node.innerHTML==="9" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 9);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="10" && sign.innerHTML==="/"){
+const arrayList=array.map(element => element / 10);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+
+else if (node.innerHTML==="0" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 0);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="1" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 1);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="2" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 2);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="3" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 3);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="4" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 4);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="5" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 5);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="6" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 6);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="7" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 7);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="8" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 8);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;} 
+else if (node.innerHTML==="9" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 9);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+else if (node.innerHTML==="10" && sign.innerHTML==="-"){
+const arrayList=array.map(element => element - 10);
+array.pop();
+array.push(arrayList);
+document.getElementById("btn").innerHTML=array;}
+
+else if (node.innerHTML==="0" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 0);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}
-else if (node.innerHTML==="1"){
+else if (node.innerHTML==="1" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 1);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}
-else if (node.innerHTML==="2"){
+else if (node.innerHTML==="2" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 2);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}
-else if (node.innerHTML==="3"){
+else if (node.innerHTML==="3" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 3);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}
-else if (node.innerHTML==="4"){
+else if (node.innerHTML==="4" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 4);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}
-else if (node.innerHTML==="5"){
+else if (node.innerHTML==="5" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 5);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}
-else if (node.innerHTML==="6"){
+else if (node.innerHTML==="6" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 6);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}
-else if (node.innerHTML==="7"){
+else if (node.innerHTML==="7" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 7);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}
-else if (node.innerHTML==="8"){
+else if (node.innerHTML==="8" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 8);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;} 
-else if (node.innerHTML==="9"){
+else if (node.innerHTML==="9" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 9);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}
-else if (node.innerHTML==="10"){
+else if (node.innerHTML==="10" && sign.innerHTML==="*"){
 const arrayList=array.map(element => element * 10);
 array.pop();
 array.push(arrayList);
 document.getElementById("btn").innerHTML=array;}}
+
+function method() {
+if (sign.innerHTML==="+") {
+sign.innerHTML="-";}
+else if (sign.innerHTML==="-") {
+sign.innerHTML="/";}
+else if (sign.innerHTML==="/") {
+sign.innerHTML="*";}
+else if (sign.innerHTML==="*") {
+sign.innerHTML="+";}}
 
 function timesTablesNode(element) {
 if (element.innerHTML==="1") {
@@ -87,7 +279,10 @@ element.innerHTML="9";}
 else if (element.innerHTML==="9") {
 element.innerHTML="10";}
 else if (element.innerHTML==="10") {
-element.innerHTML="1";}}
+element.innerHTML="0";}
+else if (element.innerHTML==="0") {
+element.innerHTML="1";}
+}
 function calculate(){
 let array = [1];
 let arrayTwo = [2];
